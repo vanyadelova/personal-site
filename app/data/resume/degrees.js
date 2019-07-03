f0acad726cec3d89bff4a10266d292f27ddc8536
vanyadelova <happy.vanya.delova@gmail.com>
2019-07-03 19:41:22 +0200

    adding education part

--- app/data/resume/degrees.js
+++ app/data/resume/degrees.js
@@ -1,15 +1,28 @@
 const degrees = [
   {
-    school: 'Stanford University',
-    degree: 'M.S. Computational and Mathematical Engineering',
-    link: 'https://stanford.edu',
-    year: 2016,
-  }, {
-    school: 'University at Buffalo',
-    degree: 'B.S. Electrical Engineering, Computer Engineering',
-    link: 'https://buffalo.edu',
-    year: 2012,
+    school: 'Codaisseur Academy',
+    degree: 'Coding Bootcamp',
+    link: 'https://codaisseur.com/',
+    year: 2018,
   },
+  {
+    school: 'Ubiqum Code Academy',
+    degree: 'Coding Bootcamp',
+    link: 'https://ubiqum.com/',
+    year: 2018,
+  },
+  {
+    school: 'Bologna University',
+    degree: 'M.S. Cultural Anthropology',
+    link: 'https://www.unibo.it/en',
+    year: 2015,
+  }, {
+    school: 'Sofia University',
+    degree: 'B.S. European Ethnology',
+    link: 'https://www.uni-sofia.bg/index.php/eng',
+    year: 2011,
+  },
+  
 ];
 
 export default degrees;
