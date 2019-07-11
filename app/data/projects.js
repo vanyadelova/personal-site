7b2f9b8a81a7c00f22723f6a196c4801ae47d4ca
vanyadelova <happy.vanya.delova@gmail.com>
2019-07-11 12:58:53 +0200

    adding project page

--- app/data/projects.js
+++ app/data/projects.js
@@ -1,11 +1,11 @@
 // TODO Add a couple lines about each project
 const data = [
   {
-    title: 'Nearest Dollar',
+    title: 'Bookstore Message Board',
     subtitle: '2015 BVP Hackathon',
     link: '/projects/nearestdollar/',
-    image: '/images/projects/nearestdollar.jpg',
-    date: '2015-11-20',
+    image: '/images/projects/Bookstore.png',
+    date: '2019-04-20',
     desc: 'Built for a social impact hackathon. '
       + 'NearestDollar connected to your bank accounts, credit cards, '
       + 'or debit cards and rounded up your purchases to donate the balance to '
@@ -14,7 +14,7 @@ const data = [
     title: 'Harvest',
     subtitle: 'Won 3rd. place in 2015 Techcrunch Disrupt SF Hackathon',
     link: 'https://devpost.com/software/harvest',
-    image: '/images/projects/harvest.jpg',
+    image: '/images/projects/CRM.png',
     date: '2015-09-20',
     desc: 'Won ~ $7000 in prizes for an advanced, low cost monitoring solution '
       + 'for crops. Harvest was designed to catch irrigation leaks, overwatering, '
@@ -23,7 +23,7 @@ const data = [
     title: 'Space Potato',
     subtitle: 'A kickstarter funded potato powered weather balloon.',
     link: 'http://www.spacepotato.org',
-    image: '/images/projects/spacepotato.jpg',
+    image: '/images/projects/movie-app.png',
     date: '2015-06-28',
     desc: 'Launched a potato battery powered weather balloon with two cameras '
       + 'and gps transponder. Resulting photos were published in a coffee table book. '
@@ -32,7 +32,7 @@ const data = [
     title: 'Cat Detector',
     subtitle: 'A convolutional neural network to classify cats! (and dogs)',
     link: '/project/catdetector/',
-    image: '/images/projects/catdetector.jpg',
+    image: '/images/projects/ping-pong.png',
     date: '2015-05-15',
     desc: 'Trained a convolutional neural network to classify between ~ 80 cats breeds. '
      + 'Over 60,000 cats were classified before server bills made the project too expensive '
