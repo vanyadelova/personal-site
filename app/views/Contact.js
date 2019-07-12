e0262d21b091271936e924440c2c4fee0756a75d
vanyadelova <happy.vanya.delova@gmail.com>
2019-07-12 19:21:55 +0200

    fixing errors

--- app/views/Contact.js
+++ app/views/Contact.js
@@ -94,7 +94,7 @@ const Contact = () => {
             onMouseEnter={() => setIsActive(false)}
             onMouseLeave={() => (idx < messages.length) && setIsActive(true)}
           >
-            <a href={validateText(message) ? `mailto:${message}@mldangelo.com` : ''}>
+            <a href={validateText(message) ? `mailto:${message}@vdelova.com` : ''}>
               <span>{message}</span>
               <span>@vdelova.com</span>
             </a>
